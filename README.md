f7d1d901459ccc9509db949778604388b5d3e47c
ImQWERTY39 <sharans.roy@gmail.com>
2022-01-23 16:36:02 +0530

    Added basic gui, added to readme, changed window title and icon

---
+++ README.md
@@ -0,0 +1,2 @@
+# File Search
+As the name says, this program is used to search for files, duh
--- src/Main/main.py
+++ src/Main/main.py
@@ -1,1 +1,18 @@
-from src.Functions.Miscellaneous import *
+from Functions.Miscellaneous import *
+from Functions.SearchFiles import *
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class MainWidgets(GridLayout):
+    pass
+
+
+class FileSearch(App):
+    def build(self):
+        self.title = "File Search"
+        self.icon = "Images\\icon.png"
+
+
+if __name__ == "__main__":
+    FileSearch().run()
