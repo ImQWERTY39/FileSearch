482b1ed10ca13de7152e5eaf5d68405b2c866a49
ImQWERTY39 <sharans.roy@gmail.com>
2022-01-23 21:57:33 +0530

    Finished file search function, threading the functionsleft

--- src/Main/Functions/Miscellaneous.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-DATE = datetime.today()
-
-def getTime() -> str:
-    return f"{DATE.year}/{DATE.month}/{DATE.day}-{DATE.hour}:{DATE.minute}"
--- src/Main/main.py
+++ src/Main/main.py
@@ -1,11 +1,43 @@
-from Functions.Miscellaneous import *
-from Functions.SearchFiles import *
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+import os
 
 
 class MainWidgets(GridLayout):
-    pass
+    def findFile(path, file_name, file_ext):
+        paths = ""
+
+        for currect_path, folders, files in os.walk(path):
+            for files_ in files:
+                f_name, f_ext = os.path.splitext(files_)
+
+                if((file_name == f_name and file_ext == file_ext) or (file_name == "*" and file_ext == f_ext) or
+                (file_name == f_name and file_ext == ".*" or file_name == "*" and file_ext == ".*")):
+                    paths += f"{currect_path}\\{f_name}{f_ext}\n"
+            
+        paths = paths.strip()
+
+        if paths == "":
+            paths = "No files found"
+        
+        return paths
+
+
+    def search(self):
+        path = self.ids.path.text
+        file = self.ids.file.text
+
+        if(not os.path.exists(path)):
+            if "Enter a valid path" in self.ids.list.text :
+                self.ids.list.text += "!"
+            
+            else:
+                self.ids.list.text = "Enter a valid path"
+        
+        else:
+            file_name, file_ext = os.path.splitext(file)
+            
+            self.ids.list.text = MainWidgets.findFile(path, file_name, file_ext)
 
 
 class FileSearch(App):
