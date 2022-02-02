53848c2617d00b42c283007d84bdcec743ba869a
ImQWERTY39 <sharans.roy@gmail.com>
2022-02-02 15:57:34 +0530

    Removed unnessesoy files

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
+from threading import Thread
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 import os
 
 
 class MainWidgets(GridLayout):
-    def findFile(path, file_name, file_ext):
-        paths = ""
+    paths = ""
+
+    def findFile(self, path, file_name, file_ext):
+        self.ids.list.text = ""
 
         for currect_path, folders, files in os.walk(path):
             for files_ in files:
@@ -13,19 +16,17 @@ class MainWidgets(GridLayout):
 
                 if((file_name == f_name and file_ext == file_ext) or (file_name == "*" and file_ext == f_ext) or
                 (file_name == f_name and file_ext == ".*" or file_name == "*" and file_ext == ".*")):
-                    paths += f"{currect_path}\\{f_name}{f_ext}\n"
-            
-        paths = paths.strip()
+                    self.ids.list.text += f"{currect_path}\\{f_name}{f_ext}\n"
 
-        if paths == "":
-            paths = "No files found"
-        
-        return paths
+        if self.ids.list.text == "":
+            self.ids.list.text = "No files found" 
 
 
     def search(self):
         path = self.ids.path.text
         file = self.ids.file.text
+
+        self.ids.list.text = ""
 
         if(not os.path.exists(path)):
             if "Enter a valid path" in self.ids.list.text :
@@ -35,15 +36,22 @@ class MainWidgets(GridLayout):
                 self.ids.list.text = "Enter a valid path"
         
         else:
+            self.ids.list.text = "Searching..."
             file_name, file_ext = os.path.splitext(file)
             
-            self.ids.list.text = MainWidgets.findFile(path, file_name, file_ext)
+            t = Thread(target = MainWidgets.findFile, args = (self, path, file_name, file_ext))
+            t.start()
+
+            self.ids.list.text = MainWidgets.paths
 
 
 class FileSearch(App):
     def build(self):
         self.title = "File Search"
         self.icon = "Images\\icon.png"
+
+    def on_stop(self):
+        os._exit(0)
 
 
 if __name__ == "__main__":
